db7a99599eefa318b990e09a8805c3dba2835cd3
IgnAMS <nanomonardes123@gmail.com>
2021-09-02 18:18:29 -0400

    fin ayudantia 1

--- Ayudantia_1/notebook_ayudantia.R
+++ Ayudantia_1/notebook_ayudantia.R
@@ -42,3 +42,63 @@ mean(v1)
 sd(v1) # Standard deviation
 
 
+# Operaciones logicas
+1 > 2
+1 < 2
+v1 > 2
+
+v1 < 2 
+v2 > 2
+
+# Operadores binarios or: |, and: &
+v1 < 2 | v2 > 2
+v1 < 2 & v2 > 2
+
+typos = c(15, TRUE, "Holaaaa")
+min(typos)
+
+Mat = c(c(1, 2, 3), c(4, 5, 6)) # Lol, los concatena
+Mat
+
+length(Mat)
+1:10
+nombres[1:2]
+
+# Factor ordena el texto, parece quees feik
+opiniones = c("Bueno", "Malo", "Neutro", "Bueno", "Malo", "Malo", "Neutro", "Neutro")
+opiniones
+opiniones <- factor(opiniones)
+opiniones
+opiniones <- factor(opiniones, levels=c("Malo", "Neutro", 'Bueno'))
+opiniones
+
+
+# Matrices
+
+m = matrix(v2)
+m1 <- matrix(v2, nrow=4, ncol=5, byrow=T)
+m1
+
+m1[1,]
+m1[,1]
+m1[1,1]
+m1[c(1, 2), c(3, 4)]
+m1[-1, ] # Saca fila 
+m1[, -1] # Saca columna
+
+rownames(m1) <- c("f1", "f2", "f3", "f4")
+colnames(m1) <- c("c1", "c2", "c3", "c4", "c5")
+m1
+
+m1[m1>5]
+m1[m1<=5]
+
+nuevafila = c(7, 8, 9, 10, 11)
+m2 = rbind(m1, nuevafila) # Agrego row, con cbind podemos agregar filas
+m2
+
+
+
+
+
+
